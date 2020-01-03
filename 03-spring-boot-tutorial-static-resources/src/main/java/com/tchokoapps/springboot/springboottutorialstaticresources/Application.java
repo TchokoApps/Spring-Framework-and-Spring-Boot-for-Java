32d34efcc118f3342012637d7f1d6ceca2e1d10d
TchokoApps <tchokoapps@gmail.com>
2020-01-03 20:24:22 +0100

    update

---
+++ 03-spring-boot-tutorial-static-resources/src/main/java/com/tchokoapps/springboot/springboottutorialstaticresources/Application.java
@@ -0,0 +1,40 @@
+package com.tchokoapps.springboot.springboottutorialstaticresources;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.view.UrlBasedViewResolver;
+import org.springframework.web.servlet.view.tiles3.TilesConfigurer;
+import org.springframework.web.servlet.view.tiles3.TilesView;
+
+@SpringBootApplication
+public class Application extends SpringBootServletInitializer {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(Application.class);
+	}
+
+	@Bean
+	public UrlBasedViewResolver tilesViewResolver() {
+		UrlBasedViewResolver urlBasedViewResolver = new UrlBasedViewResolver();
+		urlBasedViewResolver.setViewClass(TilesView.class);
+		return urlBasedViewResolver;
+	}
+
+	@Bean
+	public TilesConfigurer tilesConfigurer() {
+		TilesConfigurer tilesConfigurer = new TilesConfigurer();
+		String[] defs = {"/WEB-INF/tiles.xml"};
+		tilesConfigurer.setDefinitions(defs);
+		return tilesConfigurer;
+	}
+
+}
--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Spring-Framework-and-Spring-Boot-for-Java
+
+02-spring-boot-tutorial-tiles
+-----------------------------
+http://tiles.apache.org/framework/tutorial/index.html
